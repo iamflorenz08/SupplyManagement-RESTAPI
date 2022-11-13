b2666d4ca660f20458a3620feb002a7d039eeb40
iamflorenz08 <dejesusflorenz8@gmail.com>
2022-11-13 21:41:42 +0800

    get details update

--- controllers/SupplyController.js
+++ controllers/SupplyController.js
@@ -1,14 +1,16 @@
 const SupplyModel = require('../models/SupplyModel')
 
-const get_all_details = (req,res)=>{
-
+const get_all_details = async (req,res)=>{
+    const SupplyDetails = await SupplyModel.find();
+    if(!SupplyDetails) return res.status(200).json({message: "empty"})
+    res.status(200).json(SupplyDetails)
 }
 
 const post_detail = async(req,res) => {
     try{
         const SupplyDetail = await SupplyModel.create(req.body)
         if(!SupplyDetail) return res.status(400).json({message: "Couldn't create."})
-        
+
         res.status(200).json({message: "Success."})
     }catch{
         res.status(400).json({message: "Couldn't create."})
@@ -17,5 +19,6 @@ const post_detail = async(req,res) => {
 
 
 module.exports = {
-    post_detail
+    post_detail,
+    get_all_details
 }
--- routes/SupplyRoute.js
+++ routes/SupplyRoute.js
@@ -3,8 +3,7 @@ const route = express.Router()
 const SupplyController = require('../controllers/SupplyController')
 
 route.post('/supply', SupplyController.post_detail)
-
-// route.get('/supply/details')
+route.get('/supply/details', SupplyController.get_all_details)
 // route.get('/supply/details/:id')
 
 module.exports = route
