b397de9f581140a5a275a55efa2f4f14965f85e1
iamflorenz08 <dejesusflorenz8@gmail.com>
2022-11-19 21:19:41 +0800

    fix crash

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -9,32 +9,59 @@ const authenticateToken = async (req,res,next) =>{
     const token = authHeader && authHeader.split(' ')[1]
     const type = req.body
     if(token == null) return res.sendStatus(401)
-    
-    if(type.isGmail){
-        try{
-            const ticket = await client.verifyIdToken({
-                idToken: token,
-                audience: process.env.CLIENT_ID, 
-            });
-            const payload = ticket.getPayload();        
-            req.email = payload.email
+
+    const User = await UserModel.findOne({access_token: token})
+
+    if(User){
+        jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, data)=>{
+            if(err) return res.sendStatus(401)
+            req.email = User.email
+            req.userData = User
             next()
-        }catch(error){
-            return res.status(401)
-        }
+        })
     }
-    else{
-        const User = await UserModel.findOne({access_token: token})
-        if(User){
-            jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, data)=>{
-                if(err) return res.sendStatus(401)
-                req.email = User.email
-                req.userData = User
-                next()
-            })
-        }
+    
+    try{
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: process.env.CLIENT_ID 
+        });
+        const payload = ticket.getPayload();        
+        req.email = payload.email
+        next() 
+    }
+    catch(err){
         return res.status(401) 
-    }   
+    }
+    
+    return res.status(401) 
+    
+    
+    // if(type.isGmail){
+    //     try{
+    //         const ticket = await client.verifyIdToken({
+    //             idToken: token,
+    //             audience: process.env.CLIENT_ID, 
+    //         });
+    //         const payload = ticket.getPayload();        
+    //         req.email = payload.email
+    //         next()
+    //     }catch(error){
+    //         return res.status(401)
+    //     }
+    // }
+    // else{
+    //     const User = await UserModel.findOne({access_token: token})
+    //     if(User){
+    //         jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, data)=>{
+    //             if(err) return res.sendStatus(401)
+    //             req.email = User.email
+    //             req.userData = User
+    //             next()
+    //         })
+    //     }
+    //     return res.status(401) 
+    // }   
 }
 
 
