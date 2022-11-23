c58a5ae73507ac8d9fe564e5deb7471fb633e422
iamflorenz08 <dejesusflorenz8@gmail.com>
2022-11-23 19:55:32 +0800

    update

--- utils/NodeMailer.js
+++ utils/NodeMailer.js
@@ -2,27 +2,45 @@ require('dotenv').config()
 const nodemailer = require('nodemailer')
 const ejs = require('ejs')
 
-const sendRecoveryEmail = (email,token) => {
+const sendRecoveryEmail = async (email,token) => {
 
-    ejs.renderFile('./views/email_template.ejs', {token: token} ,async(err,data)=>{
-        let transporter = nodemailer.createTransport({
-            service: "gmail",
-            port: 465,
-            auth: {
-              user: process.env.EMAIL,
-              pass: process.env.EMAIL_PASSWORD,
-            },
-        });
+    // ejs.renderFile('./views/email_template.ejs', {token: token} , async (err,data)=>{
+    //     let transporter = nodemailer.createTransport({
+    //         service: "gmail",
+    //         port: 465,
+    //         auth: {
+    //           user: process.env.EMAIL,
+    //           pass: process.env.EMAIL_PASSWORD,
+    //         },
+    //     });
     
-        let info = await transporter.sendMail({
-            from: process.env.EMAIL, // sender address
-            to: email, // list of receivers
-            subject: "Password Recovery", // Subject line
-            html: data, // html body
-        })
+    //     let info = await transporter.sendMail({
+    //         from: process.env.EMAIL, // sender address
+    //         to: email, // list of receivers
+    //         subject: "Password Recovery", // Subject line
+    //         html: data, // html body
+    //     })
 
-        console.log("Message sent: %s", info.messageId);
+    //     console.log("Message sent: %s", info.messageId);
+    // })
+
+    let transporter = nodemailer.createTransport({
+        service: "gmail",
+        port: 465,
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+    });
+
+    let info = await transporter.sendMail({
+        from: process.env.EMAIL, // sender address
+        to: email, // list of receivers
+        subject: "Password Recovery", // Subject line
+        html: `<div>${token}</div>`, // html body
     })
+
+    console.log("Message sent: %s", info.messageId);
 }
 
 module.exports = {sendRecoveryEmail}
