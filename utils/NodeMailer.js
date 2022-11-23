7c2a751370f3e96a167db237f6493a2ec81e2557
iamflorenz08 <dejesusflorenz8@gmail.com>
2022-11-23 19:58:55 +0800

    update

--- utils/NodeMailer.js
+++ utils/NodeMailer.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const nodemailer = require('nodemailer')
 const ejs = require('ejs')
 
-const sendRecoveryEmail = async (email,token) => {
+const sendRecoveryEmail = async (email, token) => {
 
     // ejs.renderFile('./views/email_template.ejs', {token: token} , async (err,data)=>{
     //     let transporter = nodemailer.createTransport({
@@ -13,7 +13,7 @@ const sendRecoveryEmail = async (email,token) => {
     //           pass: process.env.EMAIL_PASSWORD,
     //         },
     //     });
-    
+
     //     let info = await transporter.sendMail({
     //         from: process.env.EMAIL, // sender address
     //         to: email, // list of receivers
@@ -28,8 +28,8 @@ const sendRecoveryEmail = async (email,token) => {
         service: "gmail",
         port: 465,
         auth: {
-          user: process.env.EMAIL,
-          pass: process.env.EMAIL_PASSWORD,
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASSWORD,
         },
     });
 
@@ -37,10 +37,30 @@ const sendRecoveryEmail = async (email,token) => {
         from: process.env.EMAIL, // sender address
         to: email, // list of receivers
         subject: "Password Recovery", // Subject line
-        html: `<div>${token}</div>`, // html body
+        html: `<div style="display: flex; font-family: Arial">
+        <div
+            style="text-align: center; border: 2px solid #4B5563;padding: 1rem; border-radius: 10px;  background-color: white;">
+            <div style="display: flex; align-items: center;">
+                <img src="https://i.ibb.co/qNmYXgF/smslogo.png" height="50px" width="50px">
+                <h3 style="margin-left: 20px; color: #4B5563;">Forgot your password? Let's get you new one.</h3>
+            </div>
+            <hr>
+            <div>
+                <h4 style="color: #4B5563;">Supply Management System</h4>
+            </div>
+            <hr>
+            <div style="margin: 40px 40px 30px 40px;">
+                <a href="http://www.ressupplymanagement.com/${token}"
+                    style="background-color: #1D4ED8; border: 1px solid #4B5563; padding: 15px 30px; border-radius: 10px; color: white; text-decoration: none;">
+                    Reset Password
+                </a>
+            </div>
+    
+        </div>
+    </div>`, // html body
     })
 
     console.log("Message sent: %s", info.messageId);
 }
 
-module.exports = {sendRecoveryEmail}
+module.exports = { sendRecoveryEmail }
